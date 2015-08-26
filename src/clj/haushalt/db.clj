b85e1c29002fb3de9a31ae04267e039ef6fdf898
kordano <konrad.kuehne@rocketmail.com>
2015-08-26 18:54:07 +0200

    add datomic connection

---
+++ src/clj/haushalt/db.clj
@@ -0,0 +1,51 @@
+(ns haushalt.db
+  (require [datomic.api :as d]
+           [clojure.java.io :as io])
+  (:import datomic.Util))
+
+(def db-uri-base "datomic:free://0.0.0.0:4334")
+
+(defn scratch-conn
+  "Create a connection to an anonymous, in-memory database."
+  []
+  (let [uri (str "datomic:mem://" (d/squuid))]
+    (d/delete-database uri)
+    (d/create-database uri)
+    (d/connect uri)))
+
+
+(defn persistent-conn
+  "Create connection to persistent datomic database"
+  []
+  (let [uri (str db-uri-base "/haushalt")]
+    (d/create-database uri)
+    (d/connect uri)))
+
+
+(defn initialize-db [conn path]
+  (doseq [txd (-> path io/resource io/reader Util/readAll)]
+    (d/transact conn txd))
+  :done)
+
+
+(defn get-schema-elements
+  "Get identifiers of schema elements"
+  []
+  (sort
+   (d/q '[:find [?ident ...]
+          :where [_ :db/ident ?ident]]
+        db)))
+
+
+
+(comment
+
+  (def conn (scratch-conn))
+
+  (def db (d/db conn))
+  
+  (initialize-db conn "database/schema.edn")
+
+  (get-schema-elements)
+  
+  )
